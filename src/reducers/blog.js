26c4719ee0d9eb57dda24a83be3c560a14a9e7a7
Kaushik Thiyagarajan <thrn.kaushik@gmail.com>
2019-04-07 21:33:28 +0530

    updated the author update to show diff

--- src/reducers/blog.js
+++ src/reducers/blog.js
@@ -1,12 +1,25 @@
+const updateAuthorDate=(messages)=>{
+    return messages.map(message=>{
+        let updatedDate=new Date(message.updated);
+        let currentDate=new Date();
+        let diff=Math.floor((Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()) - Date.UTC(updatedDate.getFullYear(), updatedDate.getMonth(), updatedDate.getDate()) ) /(1000 * 60 * 60 * 24));
+        message.updated=`${diff} days ago`;
+        return message;
+    })
+}
+
+
 export default function blog(state = {messages:[],token:""},action) {
     switch (action.type) {
         case "GET_INITAL_DATA":
+            action.payload.messages=updateAuthorDate(action.payload.messages);
             return {
                 ...state,
                 messages:action.payload.messages,
                 token:action.payload.pageToken
             }
         case "ADD_MESSAGES":
+            action.payload.messages=updateAuthorDate(action.payload.messages);
             return {
                 ...state,
                 messages: state.messages.concat(action.payload.messages),
