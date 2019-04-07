426f1fb0a9e36f0ecd2f1476e49992f4cf378bad
Kaushik Thiyagarajan <thrn.kaushik@gmail.com>
2019-04-07 23:00:53 +0530

    infinite scroll implemented

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,40 +1,16 @@
 import React, {Component} from 'react';
-import {fetchMessages,fetchNextMessages} from '../actions/index.js';
-import {connect} from 'react-redux';
 import Header from './Header';
 import MessageList from './MessageList';
 
 class Blog extends Component {
-    state={messages:[]}
-
-    async componentDidMount(){
-        await this.props.dispatch(fetchMessages())
-        this.setState({messages:this.props.messages})
-    }
-
-    addMessages=()=>{
-        console.log("pressed");
-        this.props.dispatch(fetchNextMessages(this.props.token))
-        this.setState({messages:this.props.messages})
-    }
-
     render(){
         return(
             <div>
                 <Header />
-                <button onClick={this.addMessages}>add more</button>
-                <MessageList messages={this.state.messages}/>
+                <MessageList />
             </div>
             )
     }
 }
-
-function mapStateToProps(state){
-    console.log("state is",state)
-    return {
-        messages:state.blog.messages,
-        token:state.blog.token
-    }
-}
   
-  export default connect(mapStateToProps)(Blog);
+  export default Blog;
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import Card from './Card'
+import React,{Component} from 'react';
+import Card from './Card';
+import {fetchMessages,fetchNextMessages} from '../actions/index.js';
+import {connect} from 'react-redux';
 
-const MessageList=(props)=>{
-    console.log(props)
-    return(
-        <div className="message-list">
-            {props.messages.map((message, index)=>(<Card message={message} key={index}/>))}
-        </div>
-    )
+class MessageList extends Component{
+    
+    state={loading:false}
+
+    addMessages=()=>{
+        this.setState({loading:true});
+        console.log("pressed");
+        this.props.dispatch(fetchNextMessages(this.props.token))
+        this.setState({loading:false})
+    }
+
+    onScroll=()=>{
+        if (
+          (window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 500) &&
+          this.props.messages.length && !this.state.loading
+        ) {
+          this.addMessages();
+        }
+      }
+
+    async componentDidMount(){
+        window.addEventListener('scroll', this.onScroll, false);
+        await this.props.dispatch(fetchMessages())
+    }
+
+    render(){
+        const {messages}=this.props
+        console.log("length is",this.props)
+        return(
+            <div className="message-list">
+                {messages.map((message, index)=>(<Card message={message} key={index}/>))}
+            </div>
+        )
+    }
 }
-export default MessageList;
+
+function mapStateToProps(state){
+    console.log("state is",state)
+    return {
+        messages:state.blog.messages,
+        token:state.blog.token
+    }
+}
+
+export default connect(mapStateToProps)(MessageList);
